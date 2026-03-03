41d13a0a644b5e3ca2ebd4fd79565cbf57c750da
guoxianzhe <guoxianzhe@users.noreply.github.com>
2026-03-03 06:24:07 +0000

    [AUTO]change package from agora to shengwang

--- example/src/renderer/examples/advanced/JoinMultipleChannel/JoinMultipleChannel.tsx
+++ example/src/renderer/examples/advanced/JoinMultipleChannel/JoinMultipleChannel.tsx
@@ -9,7 +9,7 @@ import {
   RtcStats,
   UserOfflineReasonType,
   createAgoraRtcEngine,
-} from 'agora-electron-sdk';
+} from 'shengwang-electron-sdk';
 import React, { ReactElement } from 'react';
 
 import {
--- example/src/renderer/examples/basic/VideoDecoder/VideoDecoder.tsx
+++ example/src/renderer/examples/basic/VideoDecoder/VideoDecoder.tsx
@@ -10,7 +10,7 @@ import {
   RtcStats,
   VideoSourceType,
   createAgoraRtcEngine,
-} from 'agora-electron-sdk';
+} from 'shengwang-electron-sdk';
 import React, { ReactElement } from 'react';
 
 import {
