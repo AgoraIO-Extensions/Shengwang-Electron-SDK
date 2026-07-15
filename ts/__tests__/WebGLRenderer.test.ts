1f0f474fb1fe7e0673f1c00381649ee7841bc96d
gxz <guoxianzhe@agora.io>
2026-07-15 15:17:06 +0800

    test: add colorspace round-trip coverage

--- ts/__tests__/WebGLRenderer.test.ts
+++ ts/__tests__/WebGLRenderer.test.ts
@@ -31,6 +31,31 @@ const getColorSpaceParams = (
 const limitedOffset = 16 / 255;
 const limitedScale = 255 / 219;
 
+type ColorTriplet = readonly [number, number, number];
+
+const rgbToYuv = {
+  bt601Limited: ([r, g, b]: ColorTriplet): ColorTriplet => [
+    (66 * r + 129 * g + 25 * b + 0x1080) >> 8,
+    (112 * b - 74 * g - 38 * r + 0x8080) >> 8,
+    (112 * r - 94 * g - 18 * b + 0x8080) >> 8,
+  ],
+  bt601Full: ([r, g, b]: ColorTriplet): ColorTriplet => [
+    (77 * r + 150 * g + 29 * b + 0x80) >> 8,
+    (128 * b - 85 * g - 43 * r + 0x8080) >> 8,
+    (128 * r - 107 * g - 21 * b + 0x8080) >> 8,
+  ],
+  bt709Limited: ([r, g, b]: ColorTriplet): ColorTriplet => [
+    (47 * r + 157 * g + 16 * b + 0x1080) >> 8,
+    (112 * b - 87 * g - 26 * r + 0x8080) >> 8,
+    (112 * r - 102 * g - 10 * b + 0x8080) >> 8,
+  ],
+  bt709Full: ([r, g, b]: ColorTriplet): ColorTriplet => [
+    (55 * r + 183 * g + 19 * b + 0x80) >> 8,
+    (130 * b - 101 * g - 30 * r + 0x8080) >> 8,
+    (130 * r - 119 * g - 12 * b + 0x8080) >> 8,
+  ],
+};
+
 test.each([
   {
     name: 'default BT.601 limited',
@@ -202,3 +227,53 @@ test('logs color space changes after the initial application', () => {
     'WebGLRenderer color space changed: 5:1 -> 1:2'
   );
 });
+
+test.each([
+  {
+    name: 'BT.601 limited',
+    matrix: MatrixID.MatrixidBt470bg,
+    range: RangeID.RangeidLimited,
+    encode: rgbToYuv.bt601Limited,
+  },
+  {
+    name: 'BT.601 full',
+    matrix: MatrixID.MatrixidBt470bg,
+    range: RangeID.RangeidFull,
+    encode: rgbToYuv.bt601Full,
+  },
+  {
+    name: 'BT.709 limited',
+    matrix: MatrixID.MatrixidBt709,
+    range: RangeID.RangeidLimited,
+    encode: rgbToYuv.bt709Limited,
+  },
+  {
+    name: 'BT.709 full',
+    matrix: MatrixID.MatrixidBt709,
+    range: RangeID.RangeidFull,
+    encode: rgbToYuv.bt709Full,
+  },
+])(
+  '$name RGB to YUV round trip stays within quantization error',
+  (testCase) => {
+    const sourceRgb: ColorTriplet = [200, 120, 40];
+    const [y, u, v] = testCase.encode(sourceRgb);
+    const params = getColorSpaceParams(new WebGLRenderer(), {
+      matrix: testCase.matrix,
+      range: testCase.range,
+    });
+
+    const normalizedY = params.yScale * (y / 255 - params.yOffset);
+    const normalizedU = u / 255 - 0.5;
+    const normalizedV = v / 255 - 0.5;
+    const restoredRgb = [
+      normalizedY + params.rVCoeff * normalizedV,
+      normalizedY + params.gUCoeff * normalizedU + params.gVCoeff * normalizedV,
+      normalizedY + params.bUCoeff * normalizedU,
+    ].map((value) => Math.round(value * 255));
+
+    restoredRgb.forEach((value, index) => {
+      expect(Math.abs(value - sourceRgb[index]!)).toBeLessThanOrEqual(2);
+    });
+  }
+);
